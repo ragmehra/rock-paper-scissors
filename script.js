6994475472b5e05062cff2260799209dda88fc1a
Raghav Mehra <ragmehra@hotmail.com>
2021-12-20 17:15:05 -0800

    Add an event listener to the buttons that calls playGame

--- script.js
+++ script.js
@@ -16,41 +16,53 @@ function computerPlay() {
     return computerSelection;
 }
 
-//Function that figures out what the player is playing.
-function playerPlay() {
-    let playerSelection = prompt("Rock, Paper or Scissors?", "Rock");
-    playerSelection = playerSelection.toLowerCase();
-
-    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        console.log("Pick a valid selection and make sure there are no spaces!");
-
-        playerSelection = prompt("Rock, Paper or Scissors?", "Rock");
-        playerSelection = playerSelection.toLowerCase();
+//Function that figures out what the player is playing, based on button clicked
+function playerPlay(e) {
+    let playerSelection; 
+    console.log(e);
+    if (e.target.id === 'rock'){
+        playerSelection = 'rock';
     }
-
-    console.log("Player Final selection: " + playerSelection);
+    else if (e.target.id === 'paper') {
+        playerSelection = 'paper';
+    }
+    else if (e.target.id === "scissors") {
+        playerSelection = "scissors";
+    }
+    console.log("Your selection: " + playerSelection);
     return playerSelection;
 }
 
+
 //Function that plays one game
-function playGame(playerSelection, computerSelection) {
+function playGame(e) {
+
+    let playerSelection = playerPlay(e);
+    let computerSelection = computerPlay();
     const tie = "You tied!";
     const computerWin = `The computer wins! ${computerSelection} beats ${playerSelection} `;
     const playerWin = `You win! ${playerSelection} beats ${computerSelection} `;
 
     //Check for all ties.
     if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissors" && computerSelection === "scissors")) {
-        
+        console.log(tie);
         return tie;
     }
     //Check for all computer wins
     else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")) {
+        console.log(computerWin);
         return computerWin;
     }
     //Check for all player wins
     else {
+        console.log(playerWin);
         return playerWin;
     }
 }
 
+function clickPlay(button) {
+    button.addEventListener("click", playGame);
+}
 
+let buttons = document.querySelectorAll("button");
+buttons.forEach(clickPlay);
